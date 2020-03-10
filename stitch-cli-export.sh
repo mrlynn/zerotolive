1545030563005a906f10a8f29c7dd66617240a57
Michael Lynn <merlynn@gmail.com>
2020-03-10 08:57:04 -0400

    add import/export scripts

---
+++ stitch-cli-export.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+source .env
+#stitch-cli login --api-key=$STITCH_API_PUBLIC_KEY --private-api-key=$STITCH_API_PRIVATE_KEY
+
+# change the value of --app-id=zerotolive-njchv
+stitch-cli export \
+  --app-id=$STITCH_APP_ID \
+  --include-hosting
+ 
---
+++ stitch-cli-import.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+source .env
+#stitch-cli login --api-key=$STITCH_API_PUBLIC_KEY --private-api-key=$STITCH_API_PRIVATE_KEY
+
+# change the value of --app-id=zerotolive-njchv
+stitch-cli import \
+  --app-id=$STITCH_APP_ID \
+  --include-hosting
+  --strategy=merge
+  --include-dependencies
+ 
